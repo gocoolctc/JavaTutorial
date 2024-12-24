2daba75140efaa795f2ebe691059de884e57a1c3
gocoolctc <gogulanaath.ramu@ctc-g.com.sg>
2024-12-24 14:46:12 +0800

    Add files via upload

---
+++ JavaTutorial/src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    public static String IamDog(){
+        return "I am a Dog";
+    }
+
+    public static void doStuff(){
+
+        int a = 5;
+
+        while(a==5){
+            
+        }
+    }
+}
